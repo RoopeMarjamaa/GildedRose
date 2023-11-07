a4d1aeeba20c2debd3418b2fc9949bcb08ddc5bd
RoopeMarjamaa <118440808+RoopeMarjamaa@users.noreply.github.com>
2023-11-07 20:04:05 +0200

    Update GildedRose.java

--- GildedRose/src/fi/oulu/tol/sqat/GildedRose.java
+++ GildedRose/src/fi/oulu/tol/sqat/GildedRose.java
@@ -4,216 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GildedRose {
-    private static List<Item> items;
-
-    public GildedRose() {
-        if (items == null) {
-            items = new ArrayList<Item>();
-        }
-    }
+    private static List<Item> items = new ArrayList<Item>();
 
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
-        GildedRose gildedRose = new GildedRose();
-        gildedRose.initializeItems();
-        gildedRose.updateQuality();
-    }
 
-    public void initializeItems() {
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
         items.add(new Item("Elixir of the Mongoose", 5, 7));
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+        items.add(new Item("Conjured Mana Cake", 3, 6)); 
+
+        updateQuality();
     }
 
+    public static void updateQuality() {
+        for (Item item : items) {
+            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                item.setSellIn(item.getSellIn() - 1);
+
+                if (item.getName().equals("Aged Brie")) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                } else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    // Update the sellIn value first
+                    if (item.getSellIn() < 0) {
+                        item.setQuality(0); // Quality drops to 0 after the concert
+                    } else {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+
+                            if (item.getSellIn() <= 10) {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+
+                            if (item.getSellIn() <= 5) {
+                                item.setQuality(item.getQuality() + 1);
+                            }
+                        }
+                    }
+                } else {
+                    if (item.getQuality() > 0) {
+                        item.setQuality(item.getQuality() - 1);
+
+                        if (item.getSellIn() < 0) {
+                            item.setQuality(item.getQuality() - 1);
+                        }
+                    }
+
+                    if (item.getQuality() < 0) {
+                        item.setQuality(0);
+                    }
+                }
+            }
+        }
+    }
+
+    // Constructor
+    public GildedRose() {
+        items = new ArrayList<Item>();
+    }
+
+    // Getter
     public List<Item> getItems() {
         return items;
     }
 
+    // Setter
     public void setItem(Item item) {
         items.add(item);
     }
 
+    // Update one day
     public void oneDay() {
         updateQuality();
     }
-
-    public void updateQuality() {
-        for (int i = 0; i < items.size(); i++) {
-            Item currentItem = items.get(i);
-            String itemName = currentItem.getName();
-
-            if (!"Aged Brie".equals(itemName) && !"Backstage passes to a TAFKAL80ETC concert".equals(itemName)) {
-                if (currentItem.getQuality() > 0) {
-                    currentItem.setQuality(currentItem.getQuality() - 1);
-                }
-            } else if ("Aged Brie".equals(itemName)) {
-                if (currentItem.getQuality() < 50) {
-                    currentItem.setQuality(currentItem.getQuality() + 1);
-                }
-            } else if ("Backstage passes to a TAFKAL80ETC concert".equals(itemName)) {
-                if (currentItem.getQuality() < 50) {
-                    currentItem.setQuality(currentItem.getQuality() + 1);
-
-                    if (currentItem.getSellIn() < 11 && currentItem.getQuality() < 50) {
-                        currentItem.setQuality(currentItem.getQuality() + 1);
-                    }
-
-                    if (currentItem.getSellIn() < 6 && currentItem.getQuality() < 50) {
-                        currentItem.setQuality(currentItem.getQuality() + 1);
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(itemName)) {
-                currentItem.setSellIn(currentItem.getSellIn() - 1);
-            }
-
-            if (currentItem.getSellIn() < 0) {
-                if (!"Aged Brie".equals(itemName) && !"Backstage passes to a TAFKAL80ETC concert".equals(itemName) && !"Sulfuras, Hand of Ragnaros".equals(itemName)) {
-                    if (currentItem.getQuality() > 0) {
-                        currentItem.setQuality(currentItem.getQuality() - 1);
-                    }
-                } else if ("Aged Brie".equals(itemName)) {
-                    if (currentItem.getQuality() < 50) {
-                        currentItem.setQuality(currentItem.getQuality() + 1);
-                    }
-                } else if ("Backstage passes to a TAFKAL80ETC concert".equals(itemName)) {
-                    currentItem.setQuality(0);
-                }
-            }
-        }
-    }
 }
-
-
-
-
-/* ORIGINAL CODEpackage fi.oulu.tol.sqat;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class GildedRose {
-
-	private static List<Item> items = null;
-
-	/**
-	 * @param args
-	 */
-/*	public static void main(String[] args) {
-		
-        System.out.println("OMGHAI!");
-		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
-
-        updateQuality();
-}
-
-
-	
-    public static void updateQuality()
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
-
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
-            }
-        }
-    }
-
-    //constructor
-    public GildedRose() {
-    	items = new ArrayList<Item>();
-    }
-    
-    //getter
-    public List<Item> getItems() {
-    	return items;
-    }
-    //setter
-    public void setItem(Item item) {
-    	items.add(item);
-    }
-    
-    //update one day
-    public void oneDay() {
-    	updateQuality();
-    }
-}
-  */
